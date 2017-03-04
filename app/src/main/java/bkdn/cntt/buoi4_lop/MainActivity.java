147eab6b05fb3b4c01f1922a4068fcfc408be6ca
Administrator <nttinh995@gmail.com>
2017-03-05 05:12:16 +0700

    add code

---
+++ app/src/main/java/bkdn/cntt/buoi4_lop/MainActivity.java
@@ -0,0 +1,16 @@
+package bkdn.cntt.buoi4_lop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.framlayout);
+//        setContentView(R.layout.linearlayout);
+        setContentView(R.layout.releativelayout);
+    }
+}
